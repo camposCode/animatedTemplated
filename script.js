4da4afeea9c23205f0dd655804318db869f016dd
camposCode <info@josecampos.io>
2022-04-30 15:00:31 +0200

    done

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-AOS.init();
+AOS.init(
+    {
+        delay: 200,
+        duration: 1500,
+        once: false,
+        mirror: false
+    }
+);
 function toggleNavbar(collapseID){
     document.getElementById(collapseID).classList.toggle('hidden');
     document.getElementById(collapseID).classList.toggle('block');
