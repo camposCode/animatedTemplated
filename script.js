faff9962322e774c483420e313fadff62be7e59b
camposCode <info@josecampos.io>
2022-04-30 14:01:47 +0200

    aos library added

---
+++ script.js
@@ -0,0 +1,5 @@
+AOS.init();
+function toggleNavbar(collapseID){
+    document.getElementById(collapseID).classList.toggle('hidden');
+    document.getElementById(collapseID).classList.toggle('block');
+}
